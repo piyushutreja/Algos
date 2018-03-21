a4203ffcdd917b9201c174c83723a92f01626017
piyush <piyush@piyush-Dell-System-Inspiron-N4110>
2018-03-21 23:11:20 +0530

    first and last occurrences

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -3,20 +3,95 @@ public class BinarySearch {
 	
 	public int searchFirstOccurence(int number, int array[])
 	{
-		return number;
+		int low=0; 
+		
+		int high = array.length-1;
+		int result= -1;
+		while(low<=high)
+		{
+			int mid= (high+low)/2;
+			
+		if (number==array[mid])
+		{
+			result = mid;
+			high= mid-1;
+			
+		}
+			
+		else if (number>array[mid])
+		{
+			low=mid+1;
+		}
+		else
+		{
+			high = mid-1;
+		}
+
+		}
+		
+		return result;
 		
 	}
 	
 	
 	public int searchLastOccurence(int number, int array[])
 	{
-		return number;
+		int low=0; 
+		
+		int high = array.length-1;
+		int result= -1;
+		while(low<=high)
+		{
+			int mid= (high+low)/2;
+			
+		if (number==array[mid])
+		{
+			result = mid;
+			low= mid+1;
+			
+		}
+			
+		else if (number>array[mid])
+		{
+			low=mid+1;
+		}
+		else
+		{
+			high = mid-1;
+		}
+
+		}
+		
+		return result;
 		
 	}
 	
 	public int searchAnyOccurence(int number, int array[])
 	{
-		return number;
+		int low=0; 
+		
+		int high = array.length-1;
+		while(low<=high)
+		{
+			int mid= (high+low)/2;
+			
+		if (number==array[mid])
+		{
+			return mid;
+		}
+			
+		else if (number>array[mid])
+		{
+			low=mid+1;
+		}
+		else
+		{
+			high = mid-1;
+		}
+
+		}
+		
+		return -1;
 		
 	}
 
--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,22 @@
 public class Main {
 
 	public static void main(String[] args) {
-System.out.println("Hi i am " );
 
- int array[] = {1,3,10,10,5,5,5,8,9,15};
+
+ int array[] = {1,3,6,5,5,5,8,9,10,10,10,20,25};
  
  BinarySearch binarySearch = new BinarySearch();
  
  int num = 10;
  
- int index = binarySearch.searchAnyOccurence(num, array);
+ int firstIndex = binarySearch.searchFirstOccurence(num, array);
 
- System.out.println(index+" is the index of "+num);
+ System.out.println(firstIndex+" is the first index of "+num);
+ 
+ int lastIndex = binarySearch.searchLastOccurence(num, array);
+ 
+ System.out.println(lastIndex+" is the last index of "+num);
+ 
 	}
 
 }
